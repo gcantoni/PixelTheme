0963626d54dbb596f791fef2ad25fe3a145f572f
folgore95 <giorgio.canto98@gmail.com>
2017-09-19 20:47:29 +0200

    First release

---
+++ app/src/main/java/substratum/theme/template/internal/TBOConstants.kt
@@ -0,0 +1,15 @@
+package substratum.theme.template.internal
+
+object TBOConstants {
+
+    val THEME_READY_PACKAGES = arrayOf(
+            "com.google.android.gm",
+            "com.google.android.googlequicksearchbox",
+            "com.android.vending",
+            "com.google.android.apps.plus",
+            "com.google.android.talk",
+            "com.google.android.youtube",
+            "com.google.android.inputmethod.latin"
+    )
+
+}
